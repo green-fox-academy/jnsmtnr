96756ccc293597418a5d40d3a4fcde123716cfc1
jnsmtnr <janosmaitner@gmail.com>
2018-08-03 10:42:37 +0200

    add(draw-pyramids.ts): megvan a piramis

---
+++ week-01/day-5/draw-pyramid.ts
@@ -0,0 +1,30 @@
+'use strict';
+
+let lineCount: number = 4;
+let spaces: string = "";
+let stars: string = "*";
+
+//a sornak a for ciklusa
+for (let i: number = 0; i <lineCount; i++) {
+    //a 'spaces' for ciklusa
+    for (let j: number = 1; j < lineCount - i; j++) {
+        spaces += " ";
+    }
+    //a 'stars' for ciklusa
+    for (let k: number = 0; k < i; k++) {
+        stars += "**";
+    }
+    console.log(spaces + stars);  
+    spaces = "";
+    stars = "*";  
+}
+
+// Write a program that draws a
+// pyramid like this:
+//
+// 123*
+// 12***
+// 1*****
+// *******
+//
+// The pyramid should have as many lines as lineCount is
