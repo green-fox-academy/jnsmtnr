0419e3ef73c4cac91a8b30b5d088926df402d637
jnsmtnr <janosmaitner@gmail.com>
2018-08-07 12:30:33 +0200

    add(student-counter.ts): added howManyCandies and sumOfAgeOfStudentsWithLessThan5Candies function

---
+++ week-02/day-2/student-counter.ts
@@ -0,0 +1,37 @@
+'use strict';
+
+const students: any[] = [
+  {name: 'Theodor', age: 3, candies: 2},
+  {name: 'Paul', age: 9.5, candies: 2},
+  {name: 'Mark', age: 12, candies: 5},
+  {name: 'Peter', age: 7, candies: 3},
+  {name: 'Olaf', age: 12, candies: 7},
+  {name: 'George', age: 10, candies: 1}
+];
+
+function howManyCandies(myArray) {
+    let sum: number = 0;
+    myArray.forEach(function (elem) {
+        sum += elem["candies"];
+    })
+    console.log(sum);
+};
+
+function sumOfAgeOfStudentsWithFewCandies(myArray) {
+    let sum: number = 0;
+    myArray.forEach(function (elem) {
+        if (elem["candies"] < 5) {
+            sum += elem["age"];
+        }
+    })
+    console.log(sum);
+}
+
+howManyCandies(students);
+sumOfAgeOfStudentsWithFewCandies(students);
+
+
+// create a function that takes a list of students and logs: 
+// - how many candies are owned by students
+// create a function that takes a list of students and logs:
+// - Sum of the age of people who have lass than 5 candies
