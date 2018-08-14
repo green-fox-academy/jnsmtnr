89fedd76949a47d701d49f0485429b5abd3795cb
jnsmtnr <janosmaitner@gmail.com>
2018-08-14 15:28:45 +0200

    feat(count-lines.ts): if file not found, count of lines equals zero

--- week-03/day-2/count-lines.ts
+++ week-03/day-2/count-lines.ts
@@ -4,7 +4,12 @@ declare function require(path: string): any;
 const fs = require('fs');
 
 function countLines (filename: string) {
-  return fs.readFileSync(filename, 'utf-8').split('\r\n').length;
+  try {
+    return fs.readFileSync(filename, 'utf-8').split('\r\n').length;
+  }
+  catch(error) {
+    return 0;
+  }
 }
 
 console.log('Count of lines: ' + countLines('../../../text.txt'));
