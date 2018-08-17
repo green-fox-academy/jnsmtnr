c2d35df1705aafb32c385342963a8299a2344845
jnsmtnr <janosmaitner@gmail.com>
2018-08-17 11:55:46 +0200

    feat(carpet.ts): added recursivity

--- week-03/day-5/Sierpinsky carpet/carpet.ts
+++ week-03/day-5/Sierpinsky carpet/carpet.ts
@@ -4,11 +4,25 @@ const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
 const ctx = canvas.getContext('2d');
 
 class Drawing {
-  centerPointSquare(x,y,size) {
-    ctx.fillRect(x-size/2, y-size/2, size,size);
+  centerPointSquare(x, y, size) {
+    ctx.fillRect(x - size / 2, y - size / 2, size, size);
+  }
+  nineSquares(x, y, size, i) {
+    if (i < 5) {
+      this.centerPointSquare(x, y, size / 3);
+      for (let row = -1; row <= 1; row++) {
+        for (let col = -1; col <= 1; col++) {
+          if (col != 0 || row != 0) {
+            this.nineSquares(x - col*(size / 3), y - row*(size / 3), size / 3, i + 1);
+          }
+
+        }
+      }
+
+    }
   }
 }
 
 let drawing: Drawing = new Drawing();
 
-drawing.centerPointSquare(canvas.width/2,canvas.height/2,100);
+drawing.nineSquares(canvas.width / 2, canvas.height / 2, canvas.width, 0);
