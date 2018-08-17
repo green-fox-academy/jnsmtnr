1a048b11e5611354d296b754f4400f57197fdbd5
jnsmtnr <janosmaitner@gmail.com>
2018-08-17 12:32:16 +0200

    feat: added random size and animated it

--- week-03/day-5/Sierpinsky carpet/carpet.ts
+++ week-03/day-5/Sierpinsky carpet/carpet.ts
@@ -6,24 +6,39 @@ const ctx = canvas.getContext('2d');
 class Drawing {
   centerPointSquare(x, y, size) {
     ctx.fillStyle = 'rgb(' + Math.random() * 255 + ',' + Math.random() * 255 + ',' + Math.random() * 255 + ')';
-    ctx.fillRect(x - size / 2, y - size / 2, size, size);
+    let randomSizeMul: number = Math.random() * 0.2 + 0.9;
+    ctx.fillRect(x - size * randomSizeMul / 2, y - size * randomSizeMul / 2, size * randomSizeMul, size * randomSizeMul);
   }
   nineSquares(x, y, size, i) {
     if (i < 5) {
       this.centerPointSquare(x, y, size / 3);
       for (let row = -1; row <= 1; row++) {
         for (let col = -1; col <= 1; col++) {
-          if (col != 0 || row != 0) {
+          if (!(col == 0 && row == 0)) {
             this.nineSquares(x - col * (size / 3), y - row * (size / 3), size / 3, i + 1);
           }
 
         }
       }
-
     }
+  }
+  goaPartyOn() {
+    setTimeout(function () {
+      this.nineSquares(canvas.width / 2, canvas.height / 2, canvas.width, 0);
+      this.goaPartyOn();
+    }, 1);
   }
 }
 
 let drawing: Drawing = new Drawing();
 
-drawing.nineSquares(canvas.width / 2, canvas.height / 2, canvas.width, 0);
+//drawing.goaPartyOn();
+
+function goaPartyOn() {
+  setTimeout(function () {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawing.nineSquares(canvas.width / 2, canvas.height / 2, canvas.width, 0);
+    goaPartyOn();
+  }, 50);
+}
+goaPartyOn();
