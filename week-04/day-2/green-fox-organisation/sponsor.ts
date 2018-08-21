19dedaff5fdb4564681ee4d9c11762cc2a1b331c
jnsmtnr <janosmaitner@gmail.com>
2018-08-21 15:03:09 +0200

    add(sponsor.ts): added Sponsor class, tested it

---
+++ week-04/day-2/green-fox-organisation/sponsor.ts
@@ -0,0 +1,21 @@
+import { Person } from "./person";
+
+export class Sponsor extends Person {
+  protected company: string;
+  protected hiredStudents: number;
+
+  constructor(name: string = 'Jane Doe', age: number = 30, gender: string = 'female', company: string = 'Google') {
+    super(name, age, gender);
+    this.company = company;
+    this.hiredStudents = 0;
+  }
+  introduce() {
+    console.log(`Hi, I'm ${this.name}, a ${this.age} years old ${this.gender} who represents ${this.company} and hired ${this.hiredStudents} students so far.`);
+  }
+  getGoal() {
+    console.log("My goal is: Hire brilliant junior software developers.");
+  }
+  hire() {
+    this.hiredStudents++;
+  }
+}
