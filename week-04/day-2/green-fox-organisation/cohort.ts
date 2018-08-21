88212fc18ebfb3300092718741ad0d8dd9c371e6
jnsmtnr <janosmaitner@gmail.com>
2018-08-21 15:22:58 +0200

    add(cohort.ts): Cohort class added, tested

---
+++ week-04/day-2/green-fox-organisation/cohort.ts
@@ -0,0 +1,24 @@
+import { Student } from "./student";
+import { Mentor } from "./mentor";
+
+class Cohort {
+  name: string;
+  students: Student[];
+  mentors: Mentor[];
+
+  constructor(name: string) {
+    this.name = name;
+    this.students = [];
+    this.mentors = [];
+  }
+
+  addStudent(student: Student) {
+    this.students.push(student);
+  }
+  addMentor(mentor: Mentor) {
+    this.mentors.push(mentor);
+  }
+  info() {
+    console.log(`The ${this.name} cohort has ${this.students.length} students and ${this.mentors.length} mentors.`);
+  }
+}
