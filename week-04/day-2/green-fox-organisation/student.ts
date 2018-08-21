eeb70b87e1711f9d1d47c32107ee04cb81fd56aa
jnsmtnr <janosmaitner@gmail.com>
2018-08-21 14:29:29 +0200

    add(students.ts): Student class added, tested

---
+++ week-04/day-2/green-fox-organisation/student.ts
@@ -0,0 +1,26 @@
+import { Person } from "./person";
+
+export class Student extends Person {
+  protected previousOrganization: string;
+  protected skippedDays: number;
+
+  constructor(name: string = 'Jane Doe', age: number = 30, gender: string = 'female', previousOrganization: string = 'The School of Life') {
+    super(name, age, gender);
+    this.previousOrganization = previousOrganization;
+    this.skippedDays = 0;
+  }
+
+  getGoal() {
+    console.log("My goal is: Be a junior software developer.");
+  }
+  introduce() {
+    console.log(`Hi, I'm ${this.name}, a ${this.age} years old ${this.gender} from ${this.previousOrganization} who skipped ${this.skippedDays} days from this course already.`);
+  }
+  skipDays(numberOfDays) {
+    this.skippedDays += numberOfDays;
+  }
+}
+
+let student: Student = new Student();
+student.introduce();
+student.getGoal();
