0acf0604c48e83002caff733a136f57297c0d9fb
jnsmtnr <janosmaitner@gmail.com>
2018-08-21 14:19:41 +0200

    feat(person.ts): person class finished, tested

--- week-04/day-2/green-fox-organisation/person.ts
+++ week-04/day-2/green-fox-organisation/person.ts
@@ -3,17 +3,16 @@ export class Person {
   protected age: number;
   protected gender: string;
 
-  constructor (name: string, age: number, gender: string) {
+  constructor(name: string = 'Jane Doe', age: number = 30, gender: string = 'female') {
     this.name = name;
     this.age = age;
     this.gender = gender;
   }
 
   introduce() {
-    console.log(`Hi, I'm ${this.name}, a ${this.age} years old ${this.gender}.`)
+    console.log(`Hi, I'm ${this.name}, a ${this.age} years old ${this.gender}.`);
+  }
+  getGoal() {
+    console.log("My goal is: Live for the moment!");
   }
 }
-
-let person1: Person = new Person('brian', 33, 'male');
-
-person1.introduce();
