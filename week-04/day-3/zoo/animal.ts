73666f85393448f9884153fd47df5c39a7fce69b
jnsmtnr <janosmaitner@gmail.com>
2018-08-22 15:02:35 +0200

    feat: defined some properties and methods

--- week-04/day-3/zoo/animal.ts
+++ week-04/day-3/zoo/animal.ts
@@ -1,9 +1,21 @@
 export abstract class Animal {
   protected name: string;
   protected age: number;
+  protected isHungry: boolean;
+  protected isThirsty: boolean;
+  protected isTired: boolean;
 
   getName() {
     return this.name;
+  }
+  eat() {
+    this.isHungry = false;
+  }
+  drink () {
+    this.isThirsty = false;
+  }
+  sleep () {
+    this.isTired = false;
   }
 
   abstract breed(): string;
