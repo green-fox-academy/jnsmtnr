b74b0ed0ca7f42af674afdf5bd4c9d9951b3f2fe
jnsmtnr <janosmaitner@gmail.com>
2018-08-23 15:50:37 +0200

    second version

--- week-04/day-4/anagram.test.ts
+++ week-04/day-4/anagram.test.ts
@@ -1,9 +1,11 @@
 //"The Meaning of Life" -> "The fine game of nil"
+//"The Great British Weather" -> "Harsh, bitter, wet heritage."
 
 import { test } from 'tape';
 import { anagramChecker } from './anagram';
 
 test('Is it true?', (t) => {
   t.equal(anagramChecker("The Meaning of Life","The fine game of nil"),true);
+  t.equal(anagramChecker("The Great British Weather","Harsh, bitter, wet heritage."),true);
   t.end();
 });
--- week-04/day-4/anagram.ts
+++ week-04/day-4/anagram.ts
@@ -1,6 +1,6 @@
 function anagramChecker (string1: string, string2: string): boolean {
-  let sortedString1: string = string1.toLowerCase().split('').sort().filter( (letter) => letter != ' ').join();
-  let sortedString2: string = string2.toLowerCase().split('').sort().filter( (letter) => letter != ' ').join();
+  let sortedString1: string = string1.toLowerCase().split('').sort().filter( (letter) => letter != ' ' && letter != ',' && letter != '.'&& letter != '!'&& letter != '?' && letter != "'"&& letter != '"').join();
+  let sortedString2: string = string2.toLowerCase().split('').sort().filter( (letter) => letter != ' ' && letter != ',' && letter != '.'&& letter != '!'&& letter != '?' && letter != "'"&& letter != '"').join();
   return sortedString1 === sortedString2;
 }
 
