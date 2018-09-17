5b208197e57c7df35a0e5a2c1af6acc4ab73ca42
jnsmtnr <janosmaitner@gmail.com>
2018-09-17 12:33:44 +0200

    Doubling done

--- week-07/day-1/1/app.js
+++ week-07/day-1/1/app.js
@@ -3,12 +3,25 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const PORT = 7000;
+const PORT = 8080;
 
 app.use('/assets', express.static('assets'));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.get('/doubling/', (req, res) => {
+  if (req.query.input) {
+    res.json({
+      received: req.query.input,
+      result: req.query.input * 2,
+    });
+  } else {
+    res.json({
+      error: "Please provide an input!"
+    });
+  };
 });
 
 app.listen(PORT, () => {
