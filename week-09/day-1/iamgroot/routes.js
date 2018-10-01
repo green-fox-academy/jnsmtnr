c02a244f7bcac157be105b1f007c070961981400
jnsmtnr <janosmaitner@gmail.com>
2018-10-01 12:23:44 +0200

    feat(/groot): endpoint fully implemented

--- week-09/day-1/iamgroot/routes.js
+++ week-09/day-1/iamgroot/routes.js
@@ -4,7 +4,16 @@ const express = require('express');
 const app = express();
 
 app.get('/groot', (req, res) => {
-  res.send(req.query.message);
+  if (req.query.message) {
+    res.status(200).json({
+      received: req.query.message,
+      translated: 'I am Groot',
+    })
+  } else {
+    res.status(400).json({
+      error: 'I am Groot',
+    })
+  }
 });
 
 module.exports = app;
