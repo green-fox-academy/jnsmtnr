09a042b244f4603bb5dcdec7c74d95e514c9e919
jnsmtnr <janosmaitner@gmail.com>
2018-10-02 17:39:52 +0200

    exercise 11 done

---
+++ week-09/day-2/promise/exercise11.js
@@ -0,0 +1,27 @@
+const all = (promise1, promise2) => {
+  return new Promise( (fulfill, reject) => {
+    let counter = 0;
+    let result = [];
+
+    promise1.then( (response) => {
+      result[0] = response;
+      counter++;
+
+      if (counter >= 2) {
+        fulfill(result);
+      }
+    });
+
+    promise2.then( (response) => {
+      result[1] = response;
+      counter++;
+
+      if (counter >= 2) {
+        fulfill(result);
+      }
+    });
+  });
+}
+
+all(getPromise1(), getPromise2())
+  .then(console.log);
