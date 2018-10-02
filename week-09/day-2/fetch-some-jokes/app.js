896b5408b79889e265b8c96810f0c3571848d52d
jnsmtnr <janosmaitner@gmail.com>
2018-10-02 15:11:46 +0200

    feat(index.html, app.js): added a clear button

--- week-09/day-2/fetch-some-jokes/app.js
+++ week-09/day-2/fetch-some-jokes/app.js
@@ -1,8 +1,18 @@
 const div = document.querySelector('div');
-const button = document.querySelector('button');
+const button1 = document.querySelector('#joke');
+const button2 = document.querySelector('#clear');
 
-button.addEventListener('click', (event)=> {
+button1.addEventListener('click', () => {
   fetch('http://api.icndb.com/jokes/random')
-    .then(response => response.json())
-    .then(chuck => console.log(chuck.value.joke));
-})
+    .then(response => response.json(), error => {throw new Error(error)})
+    .then(chuck => {
+      let p = document.createElement('p');
+      p.innerHTML = chuck.value.joke;
+      div.appendChild(p);
+    })
+    .catch(error => console.log(error.message));
+});
+
+button2.addEventListener('click', () => {
+  div.innerHTML = '';
+});
